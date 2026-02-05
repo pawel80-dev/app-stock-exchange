a92a2daec6eb76dfb1ba644a578e7db2e1c5b78c
Pawel <pawel80@gmail.com>
2026-02-05 21:27:32 +0100

    app_stock_update_v58

--- source/azure/function_app.py
+++ source/azure/function_app.py
@@ -53,7 +53,8 @@ def get_basic(req: func.HttpRequest) -> str:
 def get_basic(req: func.HttpRequest) -> str:
     logger.info("AZ-FUNC OS type.")
     os_type = os.name
-    az_env = os.environ["AZURE_FUNCTIONS_ENVIRONMENT"]
+    # az_env = os.environ["AZURE_FUNCTIONS_ENVIRONMENT"]
 
     # os.name = "nt" for Windows, "posix" for Linux
-    return json.dumps({"OS type": os_type, "Environment": az_env})
+    # return json.dumps({"OS type": os_type, "Environment": az_env})
+    return json.dumps({"OS type": os_type})
